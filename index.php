9151190ebfb2756d44db8c9fb8874f6ef84a6efa
nonetheless <jetrimentimentalistically@gmail.com>
2025-04-05 21:37:04 +0800

    Updated index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 <body>
     <!-- Make a simple web page with a welcome message and a button to start the app -->
     <h1>Welcome to My App</h1>
-    <p>This is a simple web application built with PHP.</p>
+    <p>This was made to be presented for our Laboratory Exercise in Azure.</p>
     <p>Click the button below to start using the app.</p>
     <form action="start.php" method="post">
-        <button type="submit">Start App</button>
+        <button type="button">Start App</button>
     </form>
     <p>If you have any questions, feel free to contact us.</p>
-    <p>Follow us on <a href="https://www.facebook.com/sambrix.perello.1">Twitter</a> for updates.</p>
+    <p>Follow us on <a href="https://www.facebook.com/sambrix.perello.1">Facebook</a> for updates.</p>
 </body>
 </html>
